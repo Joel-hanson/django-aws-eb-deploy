cd9d69ec5f0ca0bb3ff719e3e13dd9ee1d3a95fb
Joel Hanson <joelhanson025@gmail.com>
2020-03-22 23:17:09 +0530

    more flame removed

--- action-b/entrypoint.sh
+++ action-b/entrypoint.sh
@@ -11,25 +11,17 @@ pyenv activate venv
 
 if $INPUT_FLAKE8; then
     pip install flake8
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     echo "🔥🔥🔥🔥Running flake8🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     # stop the build if there are Python syntax errors or undefined names
     flake8 . --count --show-source --statistics --config $INPUT_FLAKE8_CONFIG_FILE
 else
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     echo "🔥🔥🔥🔥Skipping flake8🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
 fi
 
 if [ $? -eq 0 ]; then
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     echo "🔥🔥🔥🔥Flake8 passed🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
 else
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     echo "🔥🔥🔥🔥Flake8 failed🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     exit 1
 fi
 
@@ -42,24 +34,14 @@ if $INPUT_UNIT_TESTING; then
         echo "postgresql"
         echo `/etc/init.d/postgresql status`
     fi
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥Running unit test🔥🔥🔥🔥🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
-    echo `ls -a`
-    echo `ls -a $GITHUB_WORKSPACE`
-    echo `ls -a $GITHUB_WORKSPACE/sample_project/`
-    echo `ls -a $GITHUB_WORKSPACE/sample_project/sample_project/`
+    echo "🔥🔥🔥🔥Running unit test🔥🔥🔥🔥"
     pip install -r requirements.txt
     pip install coverage
     coverage run --source='.' manage.py test
     if [ $? -eq 0 ]; then
-        echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
         echo "🔥🔥🔥🔥Unit test ran successfully🔥🔥🔥🔥"
-        echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     else
-        echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
         echo "🔥🔥🔥🔥Unit test failed🔥🔥🔥🔥"
-        echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
         exit 1
     fi
     mkdir -p $GITHUB_WORKSPACE/output
@@ -69,45 +51,31 @@ if $INPUT_UNIT_TESTING; then
     if [ $INPUT_MIN_COVERAGE -gt 0 ]; then
         COVERAGE_RESULT=$(coverage report | grep TOTAL | awk 'N=1 {print $NF}' | sed 's/%//g')
         if [ $COVERAGE_RESULT -gt $INPUT_MIN_COVERAGE ]; then
-            echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
             echo "🔥🔥🔥🔥You have a coverage of $COVERAGE_RESULT 🔥🔥🔥🔥"
-            echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
         else
-            echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
             echo "🔥🔥🔥🔥Code coverage below allowed threshold ($COVERAGE_RESULT<$INPUT_MIN_COVERAGE)🔥🔥🔥🔥s"
-            echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
             exit 1
         fi
     fi
 
 else
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     echo "🔥🔥🔥🔥🔥🔥Skipping unit test🔥🔥🔥🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
 fi
 
 if $INPUT_SECURITY_CHECK; then
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     echo "🔥🔥🔥🔥🔥Running security check🔥🔥🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     pip install bandit
     mkdir -p $GITHUB_WORKSPACE/output
     touch $GITHUB_WORKSPACE/output/security_report.txt
     bandit -r . -o $GITHUB_WORKSPACE/output/security_report.txt -f 'txt'
 else
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     echo "🔥🔥🔥🔥🔥Skipping security check🔥🔥🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
 fi
 
 if [ $? -eq 0 ]; then
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     echo "🔥🔥🔥🔥Security check passed🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
 else
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     echo "🔥🔥🔥🔥Security check failed🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     cat $GITHUB_WORKSPACE/output/security_report.txt
     exit 1
 fi
@@ -117,12 +85,8 @@ if $INPUT_DEPLOY; then
     aws configure set aws_access_key_id $INPUT_AWS_ACCESS_KEY_ID --profile eb-cli
     aws configure set aws_secret_access_key $INPUT_AWS_SECRET_ACCESS_KEY --profile eb-cli
 
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     echo "🔥🔥🔥🔥🔥🔥🔥🔥Deploying🔥🔥🔥🔥🔥🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     eb deploy
 else
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
     echo "🔥🔥🔥🔥🔥🔥🔥Skipping deploy🔥🔥🔥🔥🔥🔥🔥🔥"
-    echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
 fi
