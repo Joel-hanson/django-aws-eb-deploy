2392a91e4407afe71b35a08c775cd642dbb0a79b
Joel Hanson <joelhanson025@gmail.com>
2020-03-22 09:02:36 +0530

    down graded the boto version

--- action-b/entrypoint.sh
+++ action-b/entrypoint.sh
@@ -9,23 +9,7 @@ eval "$(pyenv virtualenv-init -)"
 pyenv virtualenv $INPUT_PYTHON_VERSION venv
 pyenv activate venv
 
-PYTHONIOENCODING=utf-8
-LANG=en_US.UTF-8
-LC_CTYPE="en_US.UTF-8"
-LC_NUMERIC="en_US.UTF-8"
-LC_TIME="en_US.UTF-8"
-LC_COLLATE="en_US.UTF-8"
-LC_MONETARY="en_US.UTF-8"
-LC_MESSAGES="en_US.UTF-8"
-LC_PAPER="en_US.UTF-8"
-LC_NAME="en_US.UTF-8"
-LC_ADDRESS="en_US.UTF-8"
-LC_TELEPHONE="en_US.UTF-8"
-LC_MEASUREMENT="en_US.UTF-8"
-LC_IDENTIFICATION="en_US.UTF-8"
-LC_ALL="en_US.UTF-8"
-
-pip install awscli==1.16.204 awsebcli==3.15.0 colorama==0.3.9
+pip install awscli awsebcli==3.10.0 colorama==0.3.7 'botocore<1.12'
 
 if $INPUT_FLAKE8;
 then
