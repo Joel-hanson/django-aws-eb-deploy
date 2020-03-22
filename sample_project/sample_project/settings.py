e3514ee5cfa4d7ef932d2efa8233d82514dcf3eb
Joel Hanson <joelhanson025@gmail.com>
2020-03-22 16:52:41 +0530

    postgres check

--- sample_project/sample_project/settings.py
+++ sample_project/sample_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sample_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
