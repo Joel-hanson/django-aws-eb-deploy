d6dc8c60264fd3efb248cf38dcb9a05893315f9a
Joel Hanson <joelhanson025@gmail.com>
2020-03-22 08:12:17 +0530

    trial and error

--- action-b/entrypoint.sh
+++ action-b/entrypoint.sh
@@ -10,6 +10,20 @@ pyenv virtualenv $INPUT_PYTHON_VERSION venv
 pyenv activate venv
 
 PYTHONIOENCODING=utf-8
+LANG=en_US.UTF-8
+LC_CTYPE="en_US.UTF-8"
+LC_NUMERIC="en_US.UTF-8"
+LC_TIME="en_US.UTF-8"
+LC_COLLATE="en_US.UTF-8"
+LC_MONETARY="en_US.UTF-8"
+LC_MESSAGES="en_US.UTF-8"
+LC_PAPER="en_US.UTF-8"
+LC_NAME="en_US.UTF-8"
+LC_ADDRESS="en_US.UTF-8"
+LC_TELEPHONE="en_US.UTF-8"
+LC_MEASUREMENT="en_US.UTF-8"
+LC_IDENTIFICATION="en_US.UTF-8"
+LC_ALL="en_US.UTF-8"
 
 pip install awscli==1.16.204 awsebcli==3.15.0 colorama==0.3.9
 
@@ -27,11 +41,8 @@ else
     echo "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥"
 fi
 
-mkdir /root/.aws
-echo "[profile eb-cli]
-aws_access_key_id = $INPUT_AWS_ACCESS_KEY_ID
-aws_secret_access_key = $INPUT_AWS_SECRET_ACCESS_KEY
-" > /root/.aws/config
+aws configure set aws_access_key_id AKIAWH2M4QZB4N2CCMPF --profile eb-cli
+aws configure set aws_secret_access_key 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 --profile eb-cli
 
 # cd /root/$INPUT_REPOSITORY_NAME
 echo `ls`
