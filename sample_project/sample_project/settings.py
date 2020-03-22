116db4c3ae6ddbe7c74f531e3add871705367e5e
Joel Hanson <joelhanson025@gmail.com>
2020-03-22 19:23:11 +0530

    psycopg2-binary installed

--- sample_project/sample_project/settings.py
+++ sample_project/sample_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', env.str('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
